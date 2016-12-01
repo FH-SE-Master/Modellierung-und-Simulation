e888a4075ec689a8440ac4531a462c2c3838de8c
Thomas Herzog <herzog.thomas81@gmail.com>
2016-12-01 11:03:07 +0100

    further dev on hands on 3

--- Uebung_3/src/lunaLandingSolver.m
+++ Uebung_3/src/lunaLandingSolver.m
@@ -7,31 +7,22 @@ simParams   = simget(fileName);
 % test euler
 % ----------------------------------------------------------------------
 simParams.Solver = 'euler';
-% run simulation
 [T,X] = sim(fileName,100,simParams,modelParams);
 
-% get height
 h    = X.signals(1).values;
-% get min height
 minHEuler = min(h);
-
 impactIdx        = find(h < 0);
 qualityEuler = X.signals(2).values(impactIdx(1));
-
 % ----------------------------------------------------------------------
 
 % ----------------------------------------------------------------------
 % Test fix step
 % ----------------------------------------------------------------------
 simParams.Solver = 'FixedStepAuto';
-% run simulation
 [T,X] = sim(fileName,100,simParams,modelParams);
 
-% get height
 h    = X.signals(1).values;
-% get min height
 minHAuto = min(h);
-
 impactIdx   = find(h < 0);
 qualityAuto = X.signals(2).values(impactIdx(1));
 % ----------------------------------------------------------------------
@@ -40,14 +31,10 @@ qualityAuto = X.signals(2).values(impactIdx(1));
 % Test fix step
 % ----------------------------------------------------------------------
 simParams.Solver = 'VariableStepAuto';
-% run simulation
 [T,X] = sim(fileName,100,simParams,modelParams);
 
-% get height
 h    = X.signals(1).values;
-% get min height
 minHVAuto = min(h);
-
 impactIdx   = find(h < 0);
 qualityVAuto = X.signals(2).values(impactIdx(1));
 % ----------------------------------------------------------------------
--- Uebung_3/src/moonLanding.m
+++ Uebung_3/src/moonLanding.m
@@ -1,31 +1,30 @@
 cfg                   = [];
 cfg.timeSpan          = 100;
-cfg.mu                = 10;
-cfg.lambda            = 100;
-cfg.delta             = 5;
-cfg.deltaStep         = 0.1;
+cfg.mu                = 5;
+cfg.lambda            = 10;
+cfg.delta             = 10;
+cfg.deltaStep         = 1;
 cfg.simFile           = 'parametrized_lunar_landing';
 cfg.simParams         = simget(cfg.simFile);
 cfg.maxValue          = 100;
 cfg.maxUnsuccessCount = 100;
-cfg.curUnsuccessCount = 100;
-cfg.maxGenerations    = 20;
+cfg.maxGenerations    = 100;
 
 runCount          = 1;
 run               = 1;
-generationCount     = 1;
+generationCount   = 1;
 curUnsuccessCount = 1;
 curBestQuality    = 1000;
 bestQualities     = [];
+meanQualities     = [];
 population        = [];
 initSolution      = [];
 
-% [14.4130]    [13.6334]    [17.8422]    [ 99.6011]    [9.7887e-04]    []    []
 % initial candiadte
 initSolution.start1 = 14;
-initSolution.end1   = 13;
-initSolution.start2 = 17;
-initSolution.end2   = 99;
+initSolution.end1   = 20;
+initSolution.start2 = 10;
+initSolution.end2   = 15;
 population{1}       = initSolution;
 
 while (run == 1)
@@ -62,8 +61,10 @@ while (run == 1)
     
     % Modify delta depending on qualtity
     bestQuality                = qualities(1,1);
+    meanQuality                = mean(qualities);
     sizeQualities              = size(qualities);
-    bestQualities(runCount,1) = bestQuality;
+    bestQualities(runCount,1)  = bestQuality;
+    meanQualities(runCount,1)  = meanQuality(1,1);
     betterCounter              = 0;
     for i=1:1:sizeQualities(1,2)
         if curBestQuality < bestQuality
@@ -74,6 +75,11 @@ while (run == 1)
     % new best quality found
     if curBestQuality > bestQuality
         curBestQuality  = bestQuality;
+        % If we found a new best decrease unseuccess counter
+        if curUnsuccessCount > 0
+            curUnsuccessCount = curUnsuccessCount - 1;
+        end
+    % If we didn't found a new best decrease unseuccess counter
     else
         curUnsuccessCount = curUnsuccessCount + 1;
     end
@@ -87,9 +93,20 @@ while (run == 1)
     
     oldBestQuality = curBestQuality;
     % Determine if continue
-    run             = ((generationCount < cfg.maxGenerations) && (curUnsuccessCount < cfg.curUnsuccessCount));
+    run             = ((generationCount < cfg.maxGenerations) && (curUnsuccessCount < cfg.maxUnsuccessCount));
     generationCount = generationCount + 1;
     runCount        = runCount + 1;
 end
 
-plot(bestQualities);
+% Plot best result per generation
+sortedbestQualities  = sortrows(bestQualities);
+runBestQuality       = sortedbestQualities(1,1);
+sizeBestQualities    = size(bestQualities);
+xAxis                = 1:(sizeBestQualities(1,1));
+
+figure;                                
+plot(xAxis, bestQualities, xAxis, meanQualities);
+hold on                                
+xlabel('Generation');   
+ylabel('quality');                            
+legend({'beat quality/generation','mean quality/generation'})        
